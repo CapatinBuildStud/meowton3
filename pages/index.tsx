7ed09ebab7dc0bc1138a18e708139f5f81dc2ea4
CapatinBuildStud <70796275+CapatinBuildStud@users.noreply.github.com>
2023-02-18 22:54:06 -0800

    its so sexy man
    its working i think

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,6 @@
 import { Layout, Text, Page } from '@vercel/examples-ui'
 import { Chat } from '../components/Chat'
-
-function Home() {
-  return (
-    <Page className="flex flex-col gap-12">
+/*
       <section className="flex flex-col gap-6">
         <Text variant="h1">OpenAI GPT-3 text model usage example</Text>
         <Text className="text-zinc-600">
@@ -11,7 +8,10 @@ function Home() {
           Routes, and OpenAI API.
         </Text>
       </section>
-
+*/
+function Home() {
+  return (  
+    <Page className="flex flex-col gap-12">
       <section className="flex flex-col gap-3">
         <Text variant="h2">AI Chat Bot:</Text>
         <div className="lg:w-2/3">
@@ -22,6 +22,6 @@ function Home() {
   )
 }
 
-Home.Layout = Layout
+//Home.Layout = Layout
 
 export default Home
